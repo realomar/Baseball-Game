52640ede624bd3cd2273abf318a50e3ba846ac73
Omar Ahmad <eatmorerice@outlook.com>
2017-05-19 09:58:04 -0400

    updated player with fields

--- Player.java
+++ Player.java
@@ -1,33 +1,19 @@
-
-/**
- * Write a description of class Player here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Player
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Player
-     */
-    public Player()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+public class Player{
+    
+    // fields
+    private String name;
+    private double strikeOutChance;
+    private double singleChance;
+    private double doubleChance;
+    private double tripleChance;
+    private double homeRunChance;
+    
+    // constructors
+    public Player (double strikeOutChance, double singleChance, double doubleChance, double tripleChance, double homeRunChance){
+        private double strikeOutChance;
+        private double singleChance;
+        private double doubleChance;
+        private double tripleChance;
+        private double homeRunChance;
     }
 }
