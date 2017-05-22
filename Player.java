e7749fa280cc1a0fce741d76d5fcfcb7be71d826
mihalekjo <17mihalekjo@fpsct.org>
2017-05-22 09:46:33 -0400

    added methods to player class

--- Player.java
+++ Player.java
@@ -9,11 +9,39 @@ public class Player{
     private double homeRunChance;
     
     // constructors
-    public Player (double strikeOutChance, double singleChance, double doubleChance, double tripleChance, double homeRunChance){
-        private double strikeOutChance;
-        private double singleChance;
-        private double doubleChance;
-        private double tripleChance;
-        private double homeRunChance;
+    public Player (String name, double strikeOutChance, double singleChance, double doubleChance, double tripleChance, double homeRunChance){
+        this.name = name;
+        this.strikeOutChance = strikeOutChance;
+        this.singleChance = singleChance;
+        this.doubleChance = doubleChance;
+        this.tripleChance = tripleChance;
+        this.homeRunChance = homeRunChance;
     }
+    
+    //methods
+    public String getName(){
+        return name;
+    }
+    
+    public double getStrikeOutChance(){
+        return strikeOutChance;
+    }
+    
+    public double getSingleChance(){
+        return singleChance;
+    }
+    
+    public double getDoubleChance(){
+        return doubleChance;
+    }
+    
+    public double getTripleChance(){
+        return tripleChance;
+    }
+    
+    public double getHomeRunChance(){
+        return homeRunChance;
+    }
+    
+    public void Hit() 
 }
