25ad07aa5bd1c9cf3ee390d0787844fe05ca943c
mihalekjo <17mihalekjo@fpsct.org>
2017-05-23 09:35:31 -0400

    add methods to player

--- Player.java
+++ Player.java
@@ -1,19 +1,7 @@
-public class Player{
-    
-    // fields
-    private String name;
-    private double strikeOutChance;
-    private double singleChance;
-    private double doubleChance;
-    private double tripleChance;
-    private double homeRunChance;
-    
-    // constructors
-    public Player (double strikeOutChance, double singleChance, double doubleChance, double tripleChance, double homeRunChance){
-        private double strikeOutChance;
-        private double singleChance;
-        private double doubleChance;
-        private double tripleChance;
-        private double homeRunChance;
+ 
+    public int Hit(){
+        
     }
+    
+    
 }
