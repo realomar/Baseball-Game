534726f0f04f522114e8abf70e766df1f90c1a42
Omar Ahmad <eatmorerice@outlook.com>
2017-05-30 09:22:42 -0400

    Added imports to team

--- Team.java
+++ Team.java
@@ -1,4 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Team{
     //fields
     private String name;
+    
+    public Team(String name){
+        
+    }
 }
