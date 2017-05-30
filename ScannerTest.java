e6cfa175b61037092c33ea7bdea26a6b52581a09
Omar Ahmad <eatmorerice@outlook.com>
2017-05-30 09:39:18 -0400

    Update ScannerTest

--- ScannerTest.java
+++ ScannerTest.java
@@ -3,5 +3,12 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ScannerTest{
-
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("BaseballTest.csv"));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext()){
+            System.out.print(scanner.next()+"|");
+        }
+        scanner.close();
+    }
 }
