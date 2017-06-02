c01c17ade1c07dcb3810defb400f937c6ea8d61e
mihalekjo <17mihalekjo@fpsct.org>
2017-06-02 09:32:04 -0400

    team

--- Game.java
+++ Game.java
@@ -1,3 +1,15 @@
 public class Game
 {
     
+//methods
+public int getRuns(Team x)
+{
+    for(int i=0 ; i < 9 ; i++)
+    {
+    
+    //return 0;
+    //Player.Hits()/4 = runs;
+    //return runs;
+    }
+}
+    
--- Team.java
+++ Team.java
@@ -1,13 +1,22 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Team{
     //fields
     private String teamName;
-    private Player[] roster;
+    private ArrayList<Player> roster = new ArrayList<Player>();
+    
+    //constructor
     public Team(String name){
-        Player[]
+        teamName = name;
+        for( int i=0 ; i<9 ; i++)
+        {
+            Player()
+        }
+        
+        // insert csv scanner;
     }
+    
 }
 
