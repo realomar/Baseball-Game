cd4ed41cc9dc354aeca6c13c56b992e74f55f83f
mihalekjo <17mihalekjo@fpsct.org>
2017-06-06 09:26:40 -0400

    teamhits

--- Game.java
+++ Game.java
@@ -1,8 +1,20 @@
 public class Game
 {
-}
-    
+
 //methods
+
+public int teamHits(Team x)
+{
+    //Team X hits, loops through team until there is three outs, then returns amount of hits / 4
+    int strikeouts = 0;
+    int numBases = 0;
+    while(strikeouts < 3)
+    {
+        numBases += Player.Hit();
+        return numBases/4;
+    }
+}
+
 public int getRuns(Team x)
 {
     for(int i=0 ; i < 9 ; i++)
@@ -13,4 +25,4 @@ public int getRuns(Team x)
     //return runs;
     }
 }
-    
+}
